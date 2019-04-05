b65889f669d3f08f9702afb6308a423dbc069adb
wgeorge1990 <williameverettgeorge@icloud.com>
2019-04-04 21:17:45 -0400

    Renders posts from db.json file upon initial load.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", setUpPage)
 function setUpPage() {
     console.log('this is a log from the intitial load of the dom')
     addFormHandler()
+    loadTodoItems()
 }
 
 function processForm(event) {
@@ -12,6 +13,26 @@ function processForm(event) {
     let priority = document.querySelector('#priority').value
     addTodoItem(description, estTime, priority)
     event.target.reset()
+}
+
+function loadTodoItems() {
+fetch("http://localhost:3000/posts").then(res => res.json()).then(data => saveData(data))
+}
+function saveData(data) {
+    let posts = data
+    console.log(posts)
+    posts.forEach(post => renderTodoItems(post))
+}
+
+function renderTodoItems(post) {
+   
+    let tableBody = document.querySelector('tbody')
+    let html = ` <tr>
+                <td>${post.description}</td>
+                <td>${post.estimatedTime}</td>
+                <td>${post.priority}</td>
+            </tr>`
+    tableBody.innerHTML += html
 }
 
 function addTodoItem(description, estTime, priority) {
